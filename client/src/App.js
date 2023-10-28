77afcfa5fe210c1c3eb14c54b808d3803cdbe9a4
Omar4589 <omar4589@hotmail.com>
2023-10-27 20:31:44 -0500

    creates header and nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import "./index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage/WelcomePage";
+import Header from "./components/HeaderComponent/Header";
 
 function App() {
   return (
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<WelcomePage />} />
       </Routes>
