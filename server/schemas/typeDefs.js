1ae36d802fd7ea3f05731e3868db3f2cf8d7c170
Omar4589 <omar4589@hotmail.com>
2023-10-30 18:27:13 -0500

    creates getAllEscapeRooms query in resolvers and typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -25,7 +25,7 @@ const typeDefs = gql`
   type Query {
     # Because we have the context functionality in place to check a JWT and decode its data, we can use a query that will always find and return the logged in user's data
     me: User
-    EscapeRooms: [Escaperoom]
+    getAllEscapeRooms: [Escaperoom]
   }
 
   type Mutation {
