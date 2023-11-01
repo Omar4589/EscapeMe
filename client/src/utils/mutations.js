435524bf2c650cef2f7123dc6d19449b0708374e
Omar4589 <omar4589@hotmail.com>
2023-11-01 18:37:39 -0500

    updates createbooking mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -54,25 +54,13 @@ export const UPDATE_PASSWORD = gql`
 export const CREATE_BOOKING = gql`
   mutation createBooking(
     $escape_room_id: Int!
-    $escape_room_theme: String!
-    $escape_room_duration: Int!
     $date: String!
     $time: String!
   ) {
-    createBooking(
-      escape_room_id: $escape_room_id
-      escape_room_theme: $escape_room_theme
-      escape_room_duration: $escape_room_duration
-      date: $date
-      time: $time
-    ) {
+    createBooking(escape_room_id: $escape_room_id, date: $date, time: $time) {
       id
       user_id
-      user_firstName
-      user_lastName
       escape_room_id
-      escape_room_theme
-      escape_room_duration
       date
       time
       created_at
