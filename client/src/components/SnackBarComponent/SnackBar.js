2170dc15f3f7f89f8b39c1b8c03f87233f7bd725
Omar4589 <omar4589@hotmail.com>
2023-11-06 16:52:09 -0600

    creates snackbar component to display error message

---
+++ client/src/components/SnackBarComponent/SnackBar.js
@@ -0,0 +1,12 @@
+const SnackBar = ({ message }) => {
+  return (
+    <div
+      id="snackbar"
+      className="fixed top-0 right-0 left-0 bg-orange-600 text-slate-100 mt-4 flex justify-center w-11/12 mx-auto rounded shadow-lg text-lg"
+    >
+      <div className="px-5 py-3  text-center">{message} </div>
+    </div>
+  );
+};
+
+export default SnackBar;
