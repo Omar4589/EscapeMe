a3eeb837326637aa78370e1cf349f0e0eb16f823
Omar4589 <omar4589@hotmail.com>
2024-01-04 01:59:51 -0600

    updates styling for rules page

--- client/src/pages/RulesPage/RulesPage.js
+++ client/src/pages/RulesPage/RulesPage.js
@@ -2,44 +2,44 @@ import ScrollToTop from "../../components/ScrollToTopWrapper/ScrollToTopWrapper"
 
 const RulesPage = () => {
   return (
-    <div className="min-h-screen bg-zinc-950 text-slate-100 px-5 py-10">
-      <h1 className="font-semibold text-5xl text-left mb-10 underline decoration-orange-600 text-center">
+    <div className="min-h-screen bg-zinc-950 text-slate-100 px-6 py-10">
+      <h1 className="font-semibold text-4xl text-left mb-9 underline decoration-orange-600 text-center">
         Rules
       </h1>
       <ul>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           1. Please arrive at least 15 minutes before your scheduled time.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           2. Your game guide will explain the rules of your escape room and
           provide information about your escape room.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           3. The{" "}
           <span className="underline decoration-orange-600 font-bold">
             maximum
           </span>{" "}
           number of players per escape room is 4.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           4. You will have 60 minutes to complete your escape room challenge.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           5. You can request hints from your game guide at any time; there's no
           limit to the number of hints you can receive.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           6. Please respect the props and equipment in the escape rooms to
           ensure the safety and enjoyment of all participants.
         </li>
-        <li className="text-xl text-left pb-6">
+        <li className="text-lg text-left pb-6">
           7. Photography and video recording inside the escape rooms are{" "}
           <span className="underline decoration-orange-600 font-bold">
             NOT allowed
           </span>{" "}
           to maintain the mystery and integrity of the experience.
         </li>
-        <li className="text-xl text-left pb-5">
+        <li className="text-lg text-left pb-5">
           8. If you need to cancel or reschedule your booking, please contact us
           at least{" "}
           <span className="underline decoration-orange-600">
@@ -47,8 +47,11 @@ const RulesPage = () => {
           </span>{" "}
           to avoid any cancellation fees.
         </li>
-        <li className="text-2xl text-left font-bold pb-6">
-          9. Have fun and enjoy your adventure!
+        <li className="text-xl text-left pb-6">
+          9.{" "}
+          <span className="text-2xl font-bold">
+            Have fun and enjoy your adventure!{" "}
+          </span>
         </li>
       </ul>
     </div>
